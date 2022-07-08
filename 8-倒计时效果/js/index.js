d8127e80b998efad80dc4d48e0b475c5f735fdd3
xiaschao <xiaschao@qq.com>
2022-07-08 16:33:12 +0800

    修改了一处错误

--- 8-倒计时效果/js/index.js
+++ 8-倒计时效果/js/index.js
@@ -7,18 +7,28 @@ var hourTen = $('.hour-ten'); // 小时十位
 var timeCover = $('.time-cover');
 var heightCover = timeCover.clientHeight;
 
+changeTime(secondSingle, 1000);
+changeTime(secondTen, 1000 * 10);
+changeTime(minuteSingle, 1000 * 60);
+changeTime(minuteTen, 1000 * 60 * 10);
+changeTime(hourSingle, 1000 * 60 * 60);
+changeTime(hourTen, 1000 * 60 * 60 * 10);
+
 // 函数区域
 function $(selector) {
   return document.querySelector(selector);
 }
+
 function changeTime(changeDom, timer) {
-  var firstNode = changeDom.children[0];
-  console.log(firstNode);
-  changeDom.style.transition = 'top .5s linear';
-  changeDom.style.top = -heightCover + 'px';
-  changeDom.addEventListener('transitionend', function () {
-    changeDom.style.transition = '';
-    changeDom.appendChild(firstNode);
-  });
+  setInterval(function () {
+    var firstNode = changeDom.children[0];
+    // console.log(firstNode);
+    changeDom.style.transition = 'top .5s linear';
+    changeDom.style.top = -heightCover + 'px';
+    changeDom.addEventListener('transitionend', function () {
+      changeDom.style.transition = '';
+      changeDom.style.top = '0px';
+      changeDom.appendChild(firstNode);
+    });
+  }, timer);
 }
-changeTime(secondSingle);
